80450607d80dd6c95eaa7c004127a4419910823c
Abdelrahman95 <abdelrahmanfathe95@gmail.com>
2025-06-17 21:59:52 +0200

    change lib name

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
-using PolynomialMultigrid
-import PolynomialMultigrid: _element_mass_matrix!, _element_prolongator!, build_prolongator
+using FerriteMultigrid
+import FerriteMultigrid: _element_mass_matrix!, _element_prolongator!, build_prolongator
 using Test
 using Ferrite
 using Tensors
